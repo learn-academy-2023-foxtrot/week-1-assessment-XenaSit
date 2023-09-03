192d43d365a220d5184d7ceb3852518e98007a40
XenaSit <sitnikovaxena@gmail.com>
2023-09-03 10:57:59 -0700

    finnished all three. had some quastions on the way, was confused, now all is well. Live long and prosper.

--- coding-concepts.js
+++ coding-concepts.js
@@ -8,48 +8,69 @@
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
 const colors = ["tangerine", "magenta", "lilac", "daffodil"]
-// console.log(colors.push("indigo"))
+console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: too embrassed to type :D
+// b) Verify and explain: this was interesting becuase i also thought that this will add the "indigo" to the end of the array, and then i remembered that when we were doing arrays i thought that it was weired that .push, or .pop returned the index value. 
 
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2023"
-// console.log(cohort.length)
+console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 10 : )))) it will return the leangth of the string in amount of index spaces it the string is taking up.
+// b) Verify and explain: .length just asks the length i do not remember having quastions about this command, so it is a pritty easy command to see what it does.
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "o"!? For a sure thing it would return the value of the sintring's index, but than what do i know. 
+// b) Verify and explain: and yes icheese a correct answer yeay!!! However i was a little skeptical because in class we found out that in the "fruit loop" aka for loops that i-returns index and [i]-returns location (this made sense in my head and in my notes, now it actualy is again not making any sense, need to go back and look at the lecture)***
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+console.log(languages[index])
+// console.log(languages(index))
 
-// a) Your answer:
-// b) Verify and explain:
+
+// a) Your answer: "Ruby"?!!! Well it better return ruby, because by all none making sense making in my head it is starting to click. so if i returns the index and the [i] returns the location, maybe it returns the location value?
+// b) Verify and explain: well, sticks and stones will have to break something because console.log(languages(index)) is returning : TypeError: languages is not a function... hmmmmmmmmmmmm .... double the hmmmm time. 
+
+const returnMyLanguageBackToMeAndPleseMakeSense = () => {
+    return(languages[index])
+}
+console.log(returnMyLanguageBackToMeAndPleseMakeSense(index))
+
+//i think i just want to confuse myself evenmore. why [] and () return the same thing in defferent situations and returning error in others? is just syntax that i need to remember and chill?! yes... i am going to get some coffe and relax
 
 // --------------------4) What will this log?
 
-const weekendDays = ["saturday", "sunday"]
+var weekendDays = ["saturday", "sunday"]
+// this aint working
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// i wanna try something: nevermind
+// console.log(weekendDays.toUpperCase("")) 
+
+// a) Your answer: ["SATURDAY", "SUNDAY"] .toUpperCase is a built in method that needs none of my  overthinking skills, so i will just leave it at that.
+// b) Verify and explain: is this a trick quastion? i am getting an error: weekendDays.toUpperCase is not a function.? WIGO (aka. what is going on?). OVERTHINKING SKILLS AKTIVATE!!!!!
+
+// i wanna try something else: hmmmmmm... Do i need a fruit loop?!!!
+console.log(weekendDays.map(item => item.toUpperCase()))
+
+console.log(weekendDays.toString().toUpperCase())
+//nope, it didn't need a loop. It olny neeeds a loop or a function if you want to return array as is with all UPPER CASES .toUpperCase is a string method and we are trying to use the built in string method for an array... i died a little inside and rose up from ashes. Here is some S&M for you. 
+
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+// console.log(dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: i am scared to answer. NUMBER?
+// b) Verify and explain: can i say i am just a geniiiiiiiiouuuuuuuse who-cant-spell, and that is why i know this!!! no? ok, well. It asked for the dataType.length (which by all of my calculations should return 3..... appperantly it will be 4, lets not dwell on this) anyyyyyywho, the typeof is a function that just returns what type of a value of anything you are asking for. YES!YES!YES !!!! no aplause!! thank you, thank you!!!
--- interview-questions.md
+++ interview-questions.md
@@ -16,40 +16,45 @@ Researched answer:
 
 1. What is git? What is the difference between git and Github?
 
-Your answer:
+Your answer: To my limited knowledge of research 7 days ago i say that GIT is like a cloude that stores all documents that people want to save. And GitHub is one of the sites that lets you look, alter and share your document. While i am here i do have a quiation: is the GitLab also like GitHub? I am still not sure, eventhough it looks similar only with a fof logo (which i realy like).
 
-Researched answer:
+Researched answer: well... poop... it is exactly the other way around. Git is actualy a "version control system" (had to plagarize here) that lets you keep track of your code and GITHUB is the cloud storage for that code. 
 
 2. Which JavaScript operators will return a Boolean value?
 
-Your answer:
+Your answer: JS operator? Whats that??? as one might ask... not me. I completly understand that the JS operator is a code or a method that requsts something fancy and returns a simple answer like boolean. Which operator will return boolean value, thats another quastion. All the equals signs, and greater or less than, and % (this simply should be called presentage, oh no, but not in JS. this thing is some medulo or mudolo anyway, the point is it gives the remainder after devision), multiplication, devision... algebra 101 "insert here". 
+    Shortly: we use any type of equasion possobility to test if our code brings true or false and than with if-else we loop through them with joy until it becomes the true value we are looking for. And this is when magicly the true code apears out of nowhere. and let us know that the code we wrote is not a complete waste.
 
-Researched answer:
+Researched answer: Noooooooo! i am crying inside. There are only 3 oprators that test true/false (|| OR) (&& AND) (! NOT). 
+10 hrs later
+Ohhhhhh! it compares to return boolean. But what is up with ===, ==, <=, >=   ???
 
-1. What is the difference between an array's index and its value?
+3. What is the difference between an array's index and its value?
 
-Your answer:
+Your answer: OH yes! something i know. INDEX is the location of a particular thing that is in array aka. value. VALUE is a particular thing that is stored in the array. 
 
-Researched answer:
+Researched answer: Well, i am not wrong, but i cant seem to find a defenition. All searches tell me to use a loop to find out if i have a value in my array. 
 
 4. Compare and contrast arrays and strings. Please include ways in which they are the same and ways in which they are different.
 
-Your answer:
+Your answer: oooh! 
+COMPARE: They both hold values, have indexes of those values and can be manipulated.
+CONTRAST: Other than that there is nothing similar about arrays and strings. While an array is the greatest manipulatable data held within a small space, string is difficult and is simple to the point that you have to turn it into an array to manipulate it. Yes, i said it. 
 
-Researched answer:
+Researched answer: apparently an ARRAY is a data structure that hold collection of elements, while the string holds collection of charactors. 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-Your answer:
+Your answer: Driver and Navigator. (I can tell i am getting tired. The sarcasm is dying down.) driver types and is not allowed to think and type at the same time. Navigator is thinking and is not allowed to type and think at the same time. 
 
-Researched answer:
+Researched answer: Oh how wrong am i. Apparently the Driver types the code, explaines outloud what they are doing, Thinking and overthinking and takes advice from the navigator if needed. NAVIGATOR points out if the comma was misplaced, and helps the driver if they are stuck. 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Higher Order Functions:
+1. Higher Order Functions: it is a function that takes in several functions into its function or returns a function as a result. 
 
-2. Jest:
+2. Jest: a simple testing technique for JS. Some call it a "Delightfull Javascript Testing" (which i might add has nothing delightfull about it. it takes longer to write than to do it by hand, unless it is a secret tool to make me practice my spelling).
 
-3. Objects:
+3. Objects: JS datatype to store whole bunch of information.
